f52cfda4d662fde1df383936b03de2b914b6a6f3
Sahil Jaiswal <sahiljaiswal166@gmail.com>
2025-07-14 23:42:08 +0530

    Added Login in user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,26 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 
+// whenever we get the user instance from the db, we get all its model, methods, everything.
+// basically 'user' is the object so along with it we get all of its properties.
+const generateAccessAndRefreshTokens = async (userId) => {
+    try {
+        const user = await User.findById(userId);
+        const accessToken = user.generateAccessToken() // given to the user
+        const refreshToken = user.generateRefreshToken() // stored in the db, so no need to login again nd again until expired.
+
+        user.refreshToken = refreshToken // adding refToken in the user object 
+        await user.save({ validateBeforeSave: false }) // saving in the db, but it may recheck for all the fields (majorly pswd) , so - false
+
+        return { accessToken, refreshToken }
+
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating refresh and access tokens")
+    }
+}
+
+
+
 const registerUser = asyncHandler( async (req, res) => {
     // get user details from frontend
     // validation - not empty and other
@@ -87,7 +107,69 @@ const registerUser = asyncHandler( async (req, res) => {
 
 } )
 
+const loginUser = asyncHandler( async (req, res) => {
+    // req body -> data
+    // username or email
+    // find the user
+    // password check
+    // access and refresh token
+    // send cookie
+
+
+    const {email, username, password} = req.body
+
+    if(!username || !email) {
+        throw new ApiError(400, "Username or email is required")
+    }
+
+    // instance of the user from db
+    const user = await User.findOne({
+        $or: [{email}, {username}]
+    })
+
+    if(!user){
+        throw new ApiError(404, "User does not exist")
+    }
+
+    // method present in the user model
+    const isPasswordValid = await user.isPasswordCorrect(password) // 'password' from req.body comparing with the pswd of the user (the instance in the db)
+
+    if(!isPasswordValid){
+        throw new ApiError(401, "Invalid user credentials")
+    }
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(user._id)
+
+    // optional step - now getting the user who has token in its object
+    const loggedInUser = await User.findOne(user._id).select(
+        "-password -refreshToken"
+    )
+
+    // now we want to send cookies and for that we need to design some options(nothing but an obj)
+    const options = {
+        httpOnly : true,  // by default it is false, that means anyone can modify cookies (frontend)
+        secure : true  // but now cookies can be modified by the server only.
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedInUser, accessToken, refreshToken
+            },
+            "User logged In Successfully"
+        )
+    )
+
+} )
+
+
 
 export {
-    registerUser
+    registerUser,
+    loginUser
 }
